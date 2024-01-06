1494636d424d523cf60600377b7fc7a6b88daf72
Pratik Kumar Singh <pratik.singh1@indiamart.com>
2024-01-06 15:36:52 +0530

    Test Commit

--- MediCare/Home/HomeView.swift
+++ MediCare/Home/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Pratik Singh")
         }
         .padding()
     }
