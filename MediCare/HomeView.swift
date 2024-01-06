311a890f5f42dade8e1319e0dd78a8a02beef0bc
Pratik Kumar Singh <pratik.singh1@indiamart.com>
2024-01-06 15:26:02 +0530

    Launch Screen Created

---
+++ MediCare/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  MediCare
+//
+//  Created by Pratik Kumar Singh on 1/6/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Pratik Singh")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- MediCare/Others/MediCareApp.swift
+++ MediCare/Others/MediCareApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MediCareApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
