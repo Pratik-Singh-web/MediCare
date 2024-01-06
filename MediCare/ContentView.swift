264b48f4c12632df31f0a1f188932ed093a3e8bf
Pratik Kumar Singh <pratik.singh1@indiamart.com>
2024-01-06 11:54:33 +0530

    Initial Commit

---
+++ MediCare/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MediCare
+//
+//  Created by Pratik Kumar Singh on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediCare/MediCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediCareApp.swift
+//  MediCare
+//
+//  Created by Pratik Kumar Singh on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MediCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
